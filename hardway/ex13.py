52646194784b9316c9f53db044fae1172f4c7bad
gxy1998 <2352019220@qq.com>
2019-04-14 15:24:02 +0800

    modify README.md

---
+++ hardway/ex13.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# 参数，解包以及变量
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("Your first variable is: ", first)
+print("Your second variable is: ", second)
+print("Your third variable is: ", third)
+
+# 命令行里面可以这样运行：
+# python hardway/ex13.py first second third
+# python hardway/ex13.py 1 2 3
+# python hardway/ex13.py 1 a first
